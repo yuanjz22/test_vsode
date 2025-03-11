0e3235205620ba46c0265b8e25c82e88e2be25a4
“yuanjz22” <yuanjz22@mails.tsinghua.edu.cn>
2025-03-11 20:41:48 +0800

    commit message

---
+++ Qwen-api.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-zeuyoabkkqpoujthzuwyuzbwxhepbqpionulgnlisvdxseen", 
+                base_url="https://api.siliconflow.cn/v1")
+
+response = client.chat.completions.create(
+    model='deepseek-ai/DeepSeek-V2.5',
+    messages=[
+        {'role':"system",
+         'content':"你好，你是我的人工智能助手贾维斯"},
+        {'role': 'user', 
+        'content': "请分析一下***这个名字"}
+    ],
+    stream=True
+)
+
+for chunk in response:
+    print(chunk.choices[0].delta.content, end='')
